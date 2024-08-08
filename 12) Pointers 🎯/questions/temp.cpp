ab7402738eabbfb99ff5727605d5849fd670a4cd
rishibakshi <guitarguyrishi@gmail.com>
2024-08-08 19:07:17 +0530

    learned pointers

---
+++ 12) Pointers 🎯/questions/temp.cpp
@@ -0,0 +1,11 @@
+/*
+
+1) when we pass array to a function so actually a pointer to an array is passed not the actall arrray
+2) vector go by by value by defalt
+
+double pointers
+
+// use case of pointers
+// r
+
+*/
