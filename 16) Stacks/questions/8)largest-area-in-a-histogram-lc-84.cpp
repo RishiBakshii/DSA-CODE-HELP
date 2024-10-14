733b714295359c84c798b1e02ba28f6aac348879
rishibakshi <guitarguyrishi@gmail.com>
2024-10-14 14:38:46 +0530

    add comments on histogram question

--- 16) Stacks/questions/8)largest-area-in-a-histogram-lc-84.cpp
+++ 16) Stacks/questions/8)largest-area-in-a-histogram-lc-84.cpp
@@ -5,6 +5,35 @@
 #include<limits.h>
 using namespace std;
 
+
+/*
+
+In this question the approach is
+
+
+// for every histogram of a specific length, if it need to expand then we need to find the left and right limits of it's expansion right?
+
+// so we can do that with the help of next smallest element and prev smallest element calculating
+
+// as if for every histogram bar if we know the next smallest element of and prev smallest element of it , then we know it's limits of expansion
+
+// but the catch is rather than storing the element we are storing the index of them
+
+// when we have the left and right limit of every bar then we can calculate the widht 
+by using this formula
+w= next-prev+1;
+
+and then which one is having the largest width we can return that 
+
+// but why we are calulating the next and prev smallest element
+as because if we need to expand one bar's widht into others then the other bar should be either tall or of same length if it is small then how can we expand right?
+
+so smaller elements are the limit on each side
+
+and that is why we are calculating the next and prev smaller element in this question
+
+*/
+
 void printArray(vector<int>&v){
     for(auto i:v) cout<<i<<" ";
     cout<<endl;
